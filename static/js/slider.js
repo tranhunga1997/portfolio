05b72298b6c478f583936ba9a1a4261cef5a6070
tranhunga1997 <tranhunga1997@gmail.com>
2022-07-14 11:33:58 +0900

    thêm chế độ sáng/tối cho theme

--- static/js/slider.js
+++ static/js/slider.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
-        // autoplay:true,
+        autoplay:true,
         // autoplayHoverPause:true,
         margin:10,
         items: 1,
--- static/js/style.js
+++ static/js/style.js
@@ -10,3 +10,18 @@ window.addEventListener('scroll', function () {
         onTopBtn.classList.remove('open')
     }
 })
+
+// Bắt sự kiện click sreen mode
+document.querySelector('#switcherAction').addEventListener('click', function () {
+    if(document.querySelector('#dardMode').disabled) {
+        // light mode -> dard mode
+        document.querySelector('#dardMode').disabled = false
+        document.querySelector('#lightMode').disabled = true
+        this.classList.remove('switcher-active')
+    } else {
+        // dark mode -> light mode
+        document.querySelector('#lightMode').disabled = false
+        document.querySelector('#dardMode').disabled = true
+        this.classList.add('switcher-active')
+    }
+})
